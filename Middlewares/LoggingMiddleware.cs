8a769be0104923a01253f6ab94e04954e467df86
Stefano Casciarri <s18449@pjwstk.edu.pl>
2021-02-06 13:19:05 +0100

    Cw6

---
+++ Middlewares/LoggingMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication2.Services;
+
+namespace WebApplication2.Middlewares
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext, IDbService service)
+        {
+            httpContext.Request.EnableBuffering();
+
+            if (httpContext.Request != null)
+            {
+                string sciezka = httpContext.Request.Path; //"weatherforecast/cos"
+                string querystring = httpContext.Request?.QueryString.ToString();
+                string metoda = httpContext.Request.Method.ToString();
+                string bodyStr = "";
+
+                using (StreamReader reader
+                 = new StreamReader(httpContext.Request.Body, Encoding.UTF8, true, 1024, true))
+                {
+                    bodyStr = await reader.ReadToEndAsync();
+                }
+
+                //logowanie do pliku
+            }
+
+            await _next(httpContext);
+        }
+
+
+    }
+}
---
+++ Services/SqlServerDbService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Services
+{
+    public class SqlServerDbService : IDbService
+    {
+        public void CheckIndexNumber(string index)
+        {
+            SqlConnection con = new SqlConnection();
+            SqlCommand com = new SqlCommand();
+
+
+        }
+
+        //...
+    }
+}
